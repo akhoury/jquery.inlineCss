0e1400fa5810bb6127069d1c2e719c1df02d9b1c
Aziz Khoury <bentael@gmail.com>
2014-10-03 11:00:16 -0400

    Update jquery.inlineCss.js

--- jquery.inlineCss.js
+++ jquery.inlineCss.js
@@ -1,6 +1,20 @@
 
         $.fn.inlineCss = (function() {
-            var whiteSpaceRE = /^[\s\xA0]+|[\s\xA0]+$/g;
+	    var trim = (function() {
+		var re = /^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g; // mdn
+		var re1 = /[\s\uFEFF\xA0]{2, }/g;
+		return function(str) {
+			return (str || '').replace(re, '').replace(re1, ' ');
+		};
+	    })();
+
+	    var cleanStyleAttr = (function() {
+		var re = /(:|;)([\s\uFEFF\xA0]?)/g;
+		return function(style) {
+			return trim(style).replace(re, '$1');
+		};
+	    })();
+
 
             return function(property, value) {
                 if (!property)
@@ -15,7 +29,7 @@
 
                 if (value != null) {
                     this.attr('style', function(i, style) {
-                        style = $.trim((style || '').replace(new RegExp('(?![^a-z\-])' + property + '[^;]+;?', 'g'), ''));
+                        style = cleanStyleAttr(style).replace(new RegExp('(?![^a-z\-])' + property + '[^;]+;?', 'g'), '');
                         return value ? style + (style && style[style.length - 1] !== ';' ? ';' : '') + ' ' + property + ': ' + (typeof value === 'function' ? value.call(this, this.index(), this.inlineCss(property)): value) : style;
                     });
 
@@ -33,9 +47,9 @@
 
                 for (var i = 0; i < properties.length; i++) {
                     var parts = properties[i].split(':');
-                    var k = (parts[0] || '').replace(whiteSpaceRE, '').toLowerCase();
+                    var k = trim(parts[0]).toLowerCase();
                     parts.shift();
-                    var v = parts.join(':').replace(whiteSpaceRE, '');
+                    var v = trim(parts.join(':'));
 
                     if (k === property) {
                         return v;
